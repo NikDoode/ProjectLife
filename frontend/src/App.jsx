910143e12e76e84f4c58c98d841a45735c6d9cb8
Nikolai_Dudalin <nikdoodekz@gmail.coom>
2026-07-21 23:46:48 +0500

    Fix initial tree loading lint error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,10 @@ export default function App() {
   }
 
   useEffect(() => {
-    loadTree();
+    fetchItemTree()
+      .then(setItems)
+      .catch((loadError) => setError(loadError.message))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
